9beef593495f8de1856829384d040bc1dc79abc6
sublum <1227045575@qq.com>
2018-10-09 21:01:17 +0800

    测试

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -2,5 +2,6 @@ class Hello {
     public static void main(String[] args) {
         System.out.println("hello,world");
         System.out.println("第一次");
+        System.out.println("ceshi");
     }
 }
