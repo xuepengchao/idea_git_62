9e06d5e88b8f1be50a2e87d3b8511e08c073142d
sublum <1227045575@qq.com>
2018-10-09 21:09:35 +0800

    还原

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -2,6 +2,5 @@ class Hello {
     public static void main(String[] args) {
         System.out.println("hello,world");
         System.out.println("第一次");
-        System.out.println("ceshi");
     }
 }
